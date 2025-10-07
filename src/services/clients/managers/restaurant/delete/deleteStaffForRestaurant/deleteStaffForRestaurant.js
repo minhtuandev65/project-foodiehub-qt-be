e0800c9ca8b33f6f1fc00cb1b41f698b0070fa73
minhtuandev65 <huynhminhtuan13032003@gmail.com>
2025-10-07 15:41:09 +0700

    update: delete staff for restaurant 2

--- src/services/clients/managers/restaurant/delete/deleteStaffForRestaurant/deleteStaffForRestaurant.js
+++ src/services/clients/managers/restaurant/delete/deleteStaffForRestaurant/deleteStaffForRestaurant.js
@@ -1,19 +1,21 @@
 import { staffModels } from '~/models/clients/manager/staff'
+import { ROLE } from '~/utils/constants'
 
 export const deleteStaffForRestaurant = async (staffId, t) => {
     const existStaff = await staffModels.findStaffById(staffId)
 
     if (!existStaff)
         throw new ApiError(StatusCodes.NOT_FOUND, t('staff.staffNotFound'))
-    console.log(existStaff)
+    
     const newUpdateData = {
-        _destroy: true
+        _destroy: true,
+        role: ROLE.USER
     }
 
     const result = await staffModels.updateStaff(
         String(existStaff.userId),
         newUpdateData
     )
-    console.log('result', result)
+    
     return result
 }
