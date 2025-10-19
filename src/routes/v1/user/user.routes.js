5531283b4983ea286eb1117f44fff7261385e48d
lam thanh qui <lamthanhquihgs@gmail.com>
2025-10-19 14:09:30 +0700

    #43 fix code note found

--- src/routes/v1/user/user.routes.js
+++ src/routes/v1/user/user.routes.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { admin } from './admin/admin.user.routes'
+import isAuthorized from '~/middlewares/authMiddleware'
+import { controller } from '~/controllers'
 
 
 const Router = express.Router()
-
+Router.route('/getMyProfile').get(isAuthorized, controller.user.data.getMyProfile)
+Router.route('/updateMyProfile').put(isAuthorized, controller.user.update.profileUser)
 Router.use('/admin', admin)
 export const user = Router
