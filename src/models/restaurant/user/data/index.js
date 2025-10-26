399e4a1b7e8fb4d658c1b310fb1e44cc2a9ccc11
minhtuandev65 <huynhminhtuan13032003@gmail.com>
2025-10-26 09:48:33 +0700

    #52 feature/feature-detail-res-for-user

--- src/models/restaurant/user/data/index.js
+++ src/models/restaurant/user/data/index.js
@@ -1,1 +1,2 @@
 export { list } from './list/list.restaurant.user.model'
+export { detail } from './detail/detail.restaurant.user.model'
--- src/services/restaurant/user/data/index.js
+++ src/services/restaurant/user/data/index.js
@@ -1,1 +1,2 @@
 export { list } from './list/list.restaurant.user.service'
+export { detail } from './detail/detail.restaurant.user.service'
